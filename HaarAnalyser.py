d18a83a6a6483ba890fe4a40c85569fcbb859790
Rey Dela Cruz <rey.delacruz@ymail.com>
2021-09-12 01:08:28 +1200

    Improved code comments

--- HaarAnalyser.py
+++ HaarAnalyser.py
@@ -1,6 +1,6 @@
-#Model Reviewer
-import cv2
+#- import the necessary packages -#
 import numpy as np
+import cv2
 
 # Load video
 video = cv2.VideoCapture(f'data/distance_test.mp4')
@@ -15,7 +15,7 @@ params = cv2.SimpleBlobDetector_Params()
 params.minThreshold = 200
 params.maxThreshold = 255
 
-# Filter by Area.
+# Filter by Area
 params.filterByArea = True
 params.minArea = 30
 
@@ -39,18 +39,20 @@ kernel = np.array([[1,1,1],
                    [0,0,0],
                    [1,1,1]], np.uint8)
 
-counter = 0;
+# Skip early frames for demo purposes
 skip = input("Skip first N frames: ")
+counter = 0;
 while(video.isOpened()):
-    #skip early frames for demo purposes!!
+    
     counter += 1
     ret, im = video.read()
+    
     if counter > int(skip):
         
-        # Convert to grey scale
+        # Convert image to grey scale
         grey = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
         
-        # Apply denoising
+        # Apply denoising filter
         grey_denoised = cv2.fastNlMeansDenoising(grey)
         
         # Detect objects
@@ -61,24 +63,35 @@ while(video.isOpened()):
             
             for body in bodies:
                 x, y, w ,h = body
+                
+                # Draw a bounding box and label on the image
                 im = cv2.rectangle(im,(x,y ,w , h),(36,255,12),2)
-                cv2.putText(im, "target", (x,  y- 10), cv2.FONT_HERSHEY_SIMPLEX , 0.9, (36,255,12), 2)
+                cv2.putText(im, "Target", (x,  y- 10), cv2.FONT_HERSHEY_SIMPLEX , 0.9, (36,255,12), 2)
         
         # Fall back to blob detection if cascade has no detections  
         else:
-            line_frame = cv2.Canny(grey_denoised, 100,150)    
+            
+            # Detect the lines using canny
+            line_frame = cv2.Canny(grey_denoised, 100,150)
+            
+            # Fill the lines using morphology
             filled_frame = cv2.morphologyEx(line_frame, cv2.MORPH_CLOSE, kernel, iterations=5)
+            
+            # Apply thresholding to make blobs easier to detect
             retval, threshold = cv2.threshold(filled_frame, 230, 255, cv2.THRESH_BINARY_INV)
+            
+            # Detect the blobs from the processes image.
             keypoints = detector.detect(threshold)
             im = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
         
+        # Define a key to break out of the loop
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
-        if key == ord('p'):
-            cv2.waitKey(-1) #wait until any key is pressed
         
+        # Display frame
         cv2.imshow('img',im)
+        
     else:
         print("Frame skipped!")
         
--- YoloAnalyser.py
+++ YoloAnalyser.py
@@ -1,8 +1,8 @@
-# import the necessary packages
+#- import the necessary packages -#
 import numpy as np
 import cv2
 
-# load the COCO class labels our YOLO model was trained on
+# Load the class labels the YOLO model was trained on
 LABELS = open("yolo/objects.names").read().strip().split("\n")
 
 # Load config and weights
@@ -23,7 +23,6 @@ skip = input("Skip first N frames: ")
 counter = 0
 while(video.isOpened()):
     
-    #skip early frames for demo purposes!!
     counter += 1
     ret, image = video.read()
     
@@ -33,21 +32,21 @@ while(video.isOpened()):
         ln = net.getLayerNames()
         ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
         
-        # construct a blob from the input image and then perform a forward
+        # Construct a blob from the input image and then perform a forward pass
         blob = cv2.dnn.blobFromImage(image, 1 / 255.0, (416, 416),
             swapRB=True, crop=False)
         net.setInput(blob)
         layerOutputs = net.forward(ln)
 
-        # lists for detected bounding boxes, confidences, and classIDs 
+        # Lists for detected bounding boxes, confidences, and classIDs 
         boxes = []
         confidences = []
         classIDs = []
 
-        # loop over each of the layer outputs
+        # Loop over each of the layer outputs
         for output in layerOutputs:
             
-            # loop over each of the detections
+            # Loop over each of the detections
             for detection in output:
                 
                 # Extract the class ID and confidence
@@ -55,10 +54,10 @@ while(video.isOpened()):
                 classID = np.argmax(scores)
                 confidence = scores[classID]
                 
-                # filter out weak predictions by ensuring the detected   
+                # Filter out weak predictions
                 if confidence > 0.2:
                     
-                    # Store the bounding boxes, confidence and classids 
+                    # Store the bounding boxes, confidence, and classids for filtered detection 
                     box = detection[0:4] * np.array([W, H, W, H])
                     (centerX, centerY, width, height) = box.astype("int")
                     x = int(centerX - (width / 2))
@@ -68,41 +67,38 @@ while(video.isOpened()):
                     confidences.append(float(confidence))
                     classIDs.append(classID)
 
-        # apply non maximum suppression
+        # Apply non maximum suppression
         idxs = cv2.dnn.NMSBoxes(boxes, confidences, 0.2, 0.6)
 
-
-        # Check object is detected
+        # Check if a something is detected.
         if len(idxs) > 0:
             
-            # loop over the indexes we are keeping
+            # Loop over detections
             for i in idxs.flatten():
                 
-                # extract the bounding box coordinates
+                # Extract the bounding box coordinates
                 (x, y) = (boxes[i][0], boxes[i][1])
                 (w, h) = (boxes[i][2], boxes[i][3])
+                
+                # Define a colour
                 color = (0,0,255)
                 
-                #Scale the boxes slightly (Easier to see the objects)
+                # Scale the boxes slightly to make it easier to see the object
                 scaled_w = (x + w)*1.005
                 scaled_h = (y + h)*1.005
                 
-                # draw a bounding box rectangle and label on the image
+                # Draw a bounding box and label on the image
                 cv2.rectangle(image, (x, y), (int(scaled_w), int(scaled_h)), color, 1)
                 text = "{}: {:.4f}".format(LABELS[classIDs[i]], confidences[i])
-                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
-                    0.5, color, 2)
-                
-        # show the output image
+                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+
+        # Define a key to break out of the loop
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
-        if key == ord('p'):
-            cv2.waitKey(-1) #wait until any key is pressed
 
-        # #Display modified frame
+        # Display frame
         cv2.imshow('img',image)
-        # img_array.append(image)
 
     else:
         print("Skipped!")
